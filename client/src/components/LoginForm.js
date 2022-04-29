5ef504eaf8fe81295b0e3709a0df2b95b9542f21
Taylor Hernandez <95730939+TayHern@users.noreply.github.com>
2022-04-29 16:21:03 -0700

    debug for heroku

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
+import { login_user } from "../utils/mutations";
 
 import Auth from "../utils/auth";
 
@@ -14,7 +14,7 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
-  const [login, { error }] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(login_user);
 
   useEffect(() => {
     if (error) {
